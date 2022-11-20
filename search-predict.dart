ab6e662fab839edf653029360e994d3e0ac1351f
3rdUt <102663724+3rdUt@users.noreply.github.com>
2022-11-20 02:53:16 +0100

    Create search-predict.dart

---
+++ search-predict.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:provider/provider.dart';
+import 'package:boride/assistants/request_assistant.dart';
+import 'package:boride/global/global.dart';
+import 'package:boride/global/map_key.dart';
+import 'package:boride/infoHandler/app_info.dart';
+import 'package:boride/models/directions.dart';
+import 'package:boride/models/predicted_places.dart';
+import 'package:boride/widgets/progress_dialog.dart';
+
+class PlacePredictionTileDesign extends StatefulWidget {
+  final PredictedPlaces? predictedPlaces;
+
+  PlacePredictionTileDesign({this.predictedPlaces});
+
+  @override
+  State<PlacePredictionTileDesign> createState() =>
+      _PlacePredictionTileDesignState();
+}
+
+class _PlacePredictionTileDesignState extends State<PlacePredictionTileDesign> {
+  getPlaceDirectionDetails(String? placeId, context) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => ProgressDialog(
+        message: "Setting Up Drof-Off, Please wait...",
+      ),
+    );
+
+    String placeDirectionDetailsUrl =
+        "https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&key=$mapKey";
+
+    var responseApi =
+        await RequestAssistant.receiveRequest(placeDirectionDetailsUrl);
+
+    Navigator.pop(context);
+
+    if (responseApi == "Error Occurred, Failed. No Response.") {
+      return;
+    }
+
+    if (responseApi["status"] == "OK") {
+      Directions directions = Directions();
+      directions.locationName = responseApi["result"]["name"];
+      directions.locationId = placeId;
+      directions.locationLatitude =
+          responseApi["result"]["geometry"]["location"]["lat"];
+      directions.locationLongitude =
+          responseApi["result"]["geometry"]["location"]["lng"];
+
+      Provider.of<AppInfo>(context, listen: false)
+          .updateDropOffLocationAddress(directions);
+
+      setState(() {
+        userDropOffAddress = directions.locationName!;
+      });
+
+      Navigator.pop(context, "obtainedDropoff");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 3, vertical: 1.5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.grey.shade100,
+      ),
+      child: SizedBox(
+        child: GestureDetector(
+          onTap: () {
+            getPlaceDirectionDetails(widget.predictedPlaces!.place_id, context);
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Row(
+              children: [
+                const Icon(
+                  Ionicons.location,
+                  color: Colors.indigo,
+                ),
+                const SizedBox(
+                  width: 14.0,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 8.0,
+                      ),
+                      Text(
+                        widget.predictedPlaces!.main_text!,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                          fontFamily: "Brand-Regular",
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 2.0,
+                      ),
+                      Text(
+                        widget.predictedPlaces!.secondary_text!,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontFamily: "Brand-Regular",
+                          fontSize: 12.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 8.0,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
